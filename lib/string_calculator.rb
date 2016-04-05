2aeee8ee6d236f66ff0679dcb8d295859216f7fa
Yan-Yi Li <liyanyi@gmail.com>
2016-04-05 11:20:03 +0100

    added test case for 1 number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,6 @@
 class StringCalculator
   def add(string_number)
-    0
+    return 0 if string_number.empty?
+    string_number.to_i
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,5 +7,9 @@ describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(calculator.add("")).to eq 0
     end
+
+    it 'returns the same number for one number' do
+      expect(calculator.add("1")).to eq 1
+    end
   end
 end
