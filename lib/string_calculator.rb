eede96903dc124048a9cbe4860ca46848b9eab25
Yan-Yi Li <liyanyi@gmail.com>
2016-04-05 11:42:49 +0100

    added test case for 2 numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,12 @@
 class StringCalculator
   def add(string_number)
     return 0 if string_number.empty?
-    string_number.to_i
+    get_number_array(string_number).reduce(:+)
+  end
+
+  private
+
+  def get_number_array(string_number)
+    string_number.split(",").map(&:to_i)
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,8 +8,12 @@ describe StringCalculator do
       expect(calculator.add("")).to eq 0
     end
 
-    it 'returns the same number for one number' do
+    it 'returns the same number for 1 number' do
       expect(calculator.add("1")).to eq 1
+    end
+
+    it 'returns their sum for 2 numbers' do
+      expect(calculator.add("1,2")).to eq 3
     end
   end
 end
