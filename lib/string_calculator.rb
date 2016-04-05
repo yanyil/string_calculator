37d62e147ae3ab1f337678bf952f448c75046182
Yan-Yi Li <liyanyi@gmail.com>
2016-04-05 11:15:15 +0100

    added test case for empty string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,5 @@
 class StringCalculator
+  def add(string_number)
+    0
+  end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,5 +1,11 @@
 require 'string_calculator'
 
 describe StringCalculator do
+  subject(:calculator) { described_class.new }
 
+  describe '#add' do
+    it 'returns 0 for an empty string' do
+      expect(calculator.add("")).to eq 0
+    end
+  end
 end
